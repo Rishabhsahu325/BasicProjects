3598e4b05cb2594878b80f09af8c923b6fd80ed5
Rishabhsahu325 <rishabhsahu325@gmail.com>
2023-10-21 22:03:51 +0530

    Added kivy source documentation tutorials

--- KivyTutorials/kivyComicCreator/ch3Events/drawingspace.py
+++
@@ -1,6 +0,0 @@
-# File name:drawingspace.py
-from kivy.uix.relativelayout import RelativeLayout
-
-class DrawingSpace(RelativeLayout):
-	def on_children(self,instance,value):
-		self.status_bar.counter= len(self.children)
